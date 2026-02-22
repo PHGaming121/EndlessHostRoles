22070d06543aaa20cfdcfa712ba285a0fc3f376b
PHGaming121 <76562427+PHGaming121@users.noreply.github.com>
2026-02-22 07:55:29 -0300

    add perplexer role (#500)
    suggested by puffball on EHR's discord server

--- Roles/Standard/Impostor/Support/Perplexer.cs
+++ Roles/Standard/Impostor/Support/Perplexer.cs
@@ -1,27 +1,32 @@
 ﻿using AmongUs.GameOptions;
+using EHR.Modules;
 using System.Linq;
 
 namespace EHR.Roles;
 
-public class Fakeshifter : RoleBase
+public class Perplexer : RoleBase
 {
     public static bool On;
-    private const int Id = 697000;
+    private const int Id = 698000;
 
     public override bool IsEnable => On;
 
+    private static OptionItem KillCooldown;
     private static OptionItem AbilityCooldown;
     private static OptionItem AbilityDuration;
     private static OptionItem AbilityUseLimit;
-    
+    private static OptionItem AbilityUseGainWithEachKill;
+
     private byte MarkedId;
 
     public override void SetupCustomOption()
     {
         StartSetup(Id)
+            .AutoSetupOption(ref KillCooldown, 30f, new FloatValueRule(1, 120, 1), OptionFormat.Seconds)
             .AutoSetupOption(ref AbilityCooldown, 30f, new FloatValueRule(1, 120, 1), OptionFormat.Seconds)
-            .AutoSetupOption(ref AbilityDuration, 10f, new FloatValueRule(1, 120, 1), OptionFormat.Seconds, overrideName: "FakeshifterAbilityDuration")
-            .AutoSetupOption(ref AbilityUseLimit, 3f, new FloatValueRule(0, 20, 0.05f), OptionFormat.Times);
+            .AutoSetupOption(ref AbilityDuration, 10f, new FloatValueRule(1, 120, 1), OptionFormat.Seconds)
+            .AutoSetupOption(ref AbilityUseLimit, 3f, new FloatValueRule(0, 20, 0.05f), OptionFormat.None)
+            .AutoSetupOption(ref AbilityUseGainWithEachKill, 0.5f, new FloatValueRule(0f, 5f, 0.25f), OptionFormat.Times);
     }
 
     public override void Init()
@@ -41,27 +46,25 @@ public class Fakeshifter : RoleBase
         AURoleOptions.ShapeshifterCooldown = AbilityCooldown.GetInt();
         opt.SetVision(true);
     }
-
     public override bool OnShapeshift(PlayerControl shapeshifter, PlayerControl target, bool shapeshifting)
     {
+        bool animated = !Options.DisableShapeshiftAnimations.GetBool();
+
         if (MarkedId == byte.MaxValue && target.IsAlive())
         {
             MarkedId = target.PlayerId;
-            PlayerControl randomPlayer = Main.EnumerateAlivePlayerControls().Without(target).RandomElement();
-            foreach (var pc in Main.AllAlivePlayerControls)
-            {
-                if (pc.PlayerId == target.PlayerId) continue;
-                target.RpcShapeshiftDesync(randomPlayer, pc, false);
-            }
-            shapeshifter.RpcRemoveAbilityUse();
+            Main.AllPlayerSpeed[target.PlayerId] *= -1;
+            Utils.MarkEveryoneDirtySettings();
+            LateTask.New(() => shapeshifter.RpcShapeshift(shapeshifter, animated), animated ? 1.2f : 0.5f);
             LateTask.New(() =>
             {
                 if (target != null && target.IsAlive())
                 {
-                    target.RpcShapeshift(target, true);
+                    Main.AllPlayerSpeed[target.PlayerId] *= -1;
                     MarkedId = byte.MaxValue;
+                    Utils.MarkEveryoneDirtySettings();
                 }
-            }, AbilityDuration.GetFloat(), "Fakeshifter Ability Finish");
+            }, AbilityDuration.GetFloat(), "Perplexer Revert Control Invert");
         }
         return false;
     }
